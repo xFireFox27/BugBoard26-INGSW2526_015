1ffcece32114cbe965b82db20a61b3d8d4d51c69
Niccolò Cavallo <niccolocavallo@gmail.com>
2025-11-28 21:22:23 +0100

    Creata IssueDaoInterface

---
+++ backend/src/main/java/it/unina/backend/daointerface/IssueDaoInterface.java
@@ -0,0 +1,10 @@
+package it.unina.backend.daointerface;
+
+import it.unina.backend.entity.Issue;
+
+public interface IssueDaoInterface {
+    public boolean createIssue(Issue issue);
+    public Issue findIssueById(Integer id);
+    public boolean updateIssue(Issue issue);
+    public boolean deleteIssue(Integer id);
+}
