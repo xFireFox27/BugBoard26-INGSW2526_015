916c963adfb7d2c2dea6ec95e512f122ef0529c6
Niccolò Cavallo <niccolocavallo@gmail.com>
2025-11-28 19:53:25 +0100

    Creata entity Issue Ci sono i controlli sula validità dei dati a livello REST ma mancano quelli a livello backend

---
+++ backend/src/main/java/it/unina/backend/entity/Issue.java
@@ -0,0 +1,84 @@
+package it.unina.backend.entity;
+
+import java.time.OffsetDateTime;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Issue {
+    @NotNull
+    private final Integer id;
+
+    @NotBlank
+    @Size(max=100)
+    private final String title;
+
+    @NotBlank
+    @Size(max=1000)
+    private final String description;
+
+    @NotBlank
+    @Size(max=100)
+    private final String type;
+
+    @NotBlank
+    @Size(max=100)
+    private final String status;
+
+    @NotBlank
+    @Size(max=100)
+    private final String createdBy;
+
+    @NotNull
+    private final OffsetDateTime createdOn;
+
+    public Issue(Integer id, String title, String description, String type, String status, String createdBy, OffsetDateTime createdOn) {
+        if (!type.equals("Bug") &&
+            !type.equals("Documentation") &&
+            !type.equals("Question") &&
+            !type.equals("Feature")) {
+            throw new IllegalArgumentException("Il tipo deve essere 'Bug', 'Documentation', 'Question' o 'Feature'");
+        }
+        if (!status.equals("To Do") &&
+            !status.equals("In Progress") &&
+            !status.equals("Done") &&
+            !status.equals("Archived")) {
+            throw new IllegalArgumentException("Lo stato deve essere 'Bug', 'Documentation', 'Question' o 'Feature'");
+        }
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+        this.createdBy = createdBy;
+        this.createdOn = createdOn;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public OffsetDateTime getCreatedOn() {
+        return createdOn;
+    }
+}
