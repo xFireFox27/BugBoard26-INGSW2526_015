e0d1acdf394dd545ff1d9d8b9324a4c5dcd7833a
Niccolò Cavallo <niccolocavallo@gmail.com>
2025-11-28 21:51:05 +0100

    Aggiunta classe IssueDao

---
+++ backend/src/main/java/it/unina/backend/dao/IssueDao.java
@@ -0,0 +1,71 @@
+package it.unina.backend.dao;
+
+import it.unina.backend.daointerface.IssueDaoInterface;
+import it.unina.backend.entity.Issue;
+import it.unina.backend.util.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class IssueDao implements IssueDaoInterface {
+    private static IssueDao instance;
+
+    private IssueDao() {}
+
+    public static IssueDao getInstance() {
+        if (instance == null) {
+            instance = new IssueDao();
+        }
+        return instance;
+    }
+
+    @Override
+    public boolean createIssue(Issue issue) {
+        String sql = "INSERT INTO issue (issue_id, title, description, type, created_by) values (?, ?, ?, ?, ?)";
+
+        try(
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            PreparedStatement st = connection.prepareStatement(sql)
+        ) {
+            st.setInt(1, issue.getId());
+            st.setString(2, issue.getTitle());
+            st.setString(3, issue.getDescription());
+            st.setString(4, issue.getType());
+            st.setString(5, issue.getCreatedBy());
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public Issue findIssueById(Integer id) {
+        String sql = "SELECT * FROM issue WHERE issue_id = ?";
+
+        try(
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            PreparedStatement st = connection.prepareStatement(sql)
+        ) {
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()) {
+                return new Issue(
+                        rs.getInt("issue_id"),
+                        rs.getString("title"),
+                        rs.getString("description"),
+                        rs.getString("type"),
+                        rs.getString("status"),
+                        rs.getString("created_by"),
+                        rs.getObject("created_on", OffsetDateTime.class)
+                );
+            } else return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
