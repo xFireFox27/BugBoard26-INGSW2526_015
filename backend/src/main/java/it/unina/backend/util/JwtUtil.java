da9ec928ae51f0292596ad326689614d4ff461bc
Niccolò Cavallo <niccolocavallo@gmail.com>
2025-11-29 16:32:05 +0100

    Modifiche stilistiche a JwtUtil

--- backend/src/main/java/it/unina/backend/util/JwtUtil.java
+++ backend/src/main/java/it/unina/backend/util/JwtUtil.java
@@ -3,13 +3,12 @@ package it.unina.backend.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-
 import java.security.Key;
 import java.util.Date;
 
 public class JwtUtil {
     private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private static final long EXPIRATION_TIME = 86400000; // 24 ore
+    private static final long EXPIRATION_TIME = 86400000; // 24 hours
 
     private JwtUtil() {}
 
@@ -20,7 +19,7 @@ public class JwtUtil {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(SECRET_KEY)
-                .compact();
+                .compact()
+        ;
     }
-
 }
