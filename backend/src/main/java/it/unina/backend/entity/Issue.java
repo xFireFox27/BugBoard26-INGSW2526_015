d7d8b2be778071fe971c90300fefde19282de936
Niccolò Cavallo <niccolocavallo@gmail.com>
2025-11-29 17:09:42 +0100

    Modifiche stilistiche a Issue e traduzione dei commenti in inglese

--- backend/src/main/java/it/unina/backend/entity/Issue.java
+++ backend/src/main/java/it/unina/backend/entity/Issue.java
@@ -44,14 +44,22 @@ public class Issue {
             !type.equals("Documentation") &&
             !type.equals("Question") &&
             !type.equals("Feature")) {
-            throw new IllegalArgumentException("Il tipo deve essere 'Bug', 'Documentation', 'Question' o 'Feature'");
+            throw new IllegalArgumentException(
+                "Attempt to insert an invalid type: " +
+                type +
+                ".\nType must be: Bug, Documentation, Question or Feature."
+            );
         }
 
         if (!status.equals("To Do") &&
             !status.equals("In Progress") &&
             !status.equals("Done") &&
             !status.equals("Archived")) {
-            throw new IllegalArgumentException("Lo stato deve essere 'Done', 'In Progress', 'Done' o 'Archived'");
+            throw new IllegalArgumentException(
+                "Attempt to insert an invalid status: " +
+                status +
+                ".\nStatus must be: To Do, In Progress, Done or Archived."
+            );
         }
 
         this.id = id;
