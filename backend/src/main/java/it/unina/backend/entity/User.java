6ec59c81bd5470e4f07e99fe67b486642e303772
Niccolò Cavallo <niccolocavallo@gmail.com>
2025-11-29 17:03:47 +0100

    Piccole modifiche logiche e di stile a User

--- backend/src/main/java/it/unina/backend/entity/User.java
+++ backend/src/main/java/it/unina/backend/entity/User.java
@@ -1,41 +1,59 @@
 package it.unina.backend.entity;
 
 import java.time.OffsetDateTime;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class User{
-    @NotNull
+    @NotBlank
     @Size(max = 100)
     private String email;
-    @NotNull
+
+    @NotBlank
     @Size(max = 100)
     private String username;
-    @NotNull
+
+    @NotBlank
     @Size(max = 100)
     private String passwordHash;
-    @NotNull
+
+    @NotBlank
     @Size(max = 100)
     private String name;
-    @NotNull
+
+    @NotBlank
     @Size(max = 100)
     private String surname;
-    @NotNull
+
+    @NotBlank
     @Size(max = 100)
     private String role;
+
     @NotNull
     private OffsetDateTime createdOn;
 
-    public User(String email, String username, String passwordHash, String name, String surname, String role, OffsetDateTime createdOn)  {
-        if(role.equals("Admin") || role.equals("Normal") || role.equals("External")){
-            this.role = role;
+    public User(String email,
+                String username,
+                String passwordHash,
+                String name,
+                String surname,
+                String role,
+                OffsetDateTime createdOn)  {
+
+        if(!role.equals("Admin") && !role.equals("Normal") && !role.equals("External")) {
+            throw new IllegalArgumentException("Attempt to insert an invalid role: " +
+                                               role +
+                                               ".\nRole must be: Admin, Normal or External.")
+            ;
         }
-        else throw new IllegalArgumentException("attempt to insert an invalid role: " + role + " role must be: admin, normal or external.");
+
         this.email = email;
         this.username = username;
         this.passwordHash = passwordHash;
         this.name = name;
         this.surname = surname;
+        this.role = role;
         this.createdOn = createdOn;
     }
 
