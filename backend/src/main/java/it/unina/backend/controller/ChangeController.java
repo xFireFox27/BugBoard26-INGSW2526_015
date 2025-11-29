49519783760c7343b12f4f285546afca08ba2e2c
Carmine Amato <carmineam.04@gmail.com>
2025-11-29 20:56:15 +0100

    Aggiunte classi per validazione token JWT e aggiunta verifica token in ChangeController

--- backend/src/main/java/it/unina/backend/controller/ChangeController.java
+++ backend/src/main/java/it/unina/backend/controller/ChangeController.java
@@ -2,37 +2,39 @@ package it.unina.backend.controller;
 
 import it.unina.backend.dao.ChangeDao;
 import it.unina.backend.entity.Change;
-import it.unina.backend.entity.Issue;
+import it.unina.backend.util.RequireJWTAuthentication;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 
 import java.util.List;
 import java.sql.SQLException;
 
 @Path("/changes")
+@RequireJWTAuthentication
 public class ChangeController {
 
     private final ChangeDao changeDao = ChangeDao.getInstance();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getChanges(@QueryParam("issue_id") Integer issueId) {
+    public Response getChanges(@Context SecurityContext securityContext,
+                               @QueryParam("issue_id") Integer issueId) {
         if (issueId == null) {
             return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("'issue_id' must be provided")
+                    .entity("{\"error\": \"'issue_id' must be provided\"}")
                     .build();
         }
 
         try {
-            // Qui avviene l'auto-unboxing da Integer a int se il DAO vuole int
             List<Change> changes = changeDao.findChangesByIssueId(issueId);
             return Response.ok(changes).build();
 
         } catch (SQLException e) {
             e.printStackTrace();
-            return Response.serverError().entity("Errore Database").build();
+            return Response.serverError().entity("{\"error\": \"Errore Database\"}").build();
         }
     }
-
 }
